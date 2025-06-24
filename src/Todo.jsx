17f3a0122d2b594f6965faef72dbea474f25c3d5
arajitdev <arajitdebnathofficial@gmail.com>
2025-06-24 13:19:01 +0530

    delete button added

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -119,7 +119,7 @@ const Todo = ()=>{
                                 {ele.importanceLevel}
                             </h6>
                             <div onClick={()=>deleteTask(idx)}  className="delete">
-                                <img src="./src/assets/delete.png" width={24} alt="" />
+                                <img src="./dist/assets/delete.png" width={24} alt="" />
                             </div>
                             
                             </div>
